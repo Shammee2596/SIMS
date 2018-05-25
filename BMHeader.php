bbcc2340fbebfd43215165d63c38de676317a5b4
Sumaya Akther Shammee <37858535+Shammee2596@users.noreply.github.com>
2018-05-25 21:37:57 +0600

    changed codes

---
+++ BMHeader.php
@@ -0,0 +1,96 @@
+<?php
+  			session_start();
+  			if(!isset($_SESSION["u_id"])) {
+  				//echo "not ok";
+  				header("Location: ../index.php? login=waaa");
+  			} 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<title></title>
+  		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="http://www.du.ac.bd/assets/css/animate.min.css" rel="stylesheet">
+ 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+  		<link rel="stylesheet" type="text/css" href="../cssFile/home.css">
+  		<link rel="stylesheet" type="text/css" href="../cssFile/systeamAdmin1.css">
+  		<link rel="stylesheet" type="text/css" href="../cssFile/notice.css">
+	</head>
+	
+	<body>
+		<div class="container-fulid">
+			<div class="row">
+				<div class="head">
+					<div class="col-lg-2">
+						<div class="menu1" style="text-align: center; padding-top: 25px;">Board Member</div>
+					</div>
+
+					<div class="col-lg-10">
+		   				<div class="col-lg-2">
+ 							<div class="option">
+								<div class="col-lg-1">
+									<a href="../index.php">
+   							 			<span class="glyphicon glyphicon-home" >Home</span>
+   							 		</a> 
+								</div>
+							</div>
+						</div>	
+						<div class="col-lg-7"></div>
+						<!-- <div class="col-lg-4">
+							<form class="form-inline">
+								<div class="form-group" id="search">
+									<input type="text" placeholder="Search.." name="search" id="searchTxt">
+   									<button type="submit" class="btn btn-default">
+          								<span class="glyphicon glyphicon-search"></span> Search 
+        							</button>									
+								</div>
+        					</form>
+						</div> -->
+						<div class="col-lg-1"></div>
+						<div class="col-lg-2">
+							<form action= "../Authentication/logoutTest.php" method="POST">
+								<div class="form-group" id="logout">
+									<button type="submit" class="btn btn-default">
+	          							<span class="glyphicon glyphicon-log-out"></span> Logout 
+        							</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2" id="left-column">
+					<div class="container">
+						<div>
+							<img src="../icon/av.png" alt="Avatar" id="image">
+							<div id="imageTxt"><?php echo $_SESSION["u_id"]; ?></div>	
+						</div>
+						<!-- <div class ="menu">
+							<span class="glyphicon glyphicon-menu-hamburger">Menu</span>
+						</div> -->
+
+						<div class="menu1">
+							<a href="createNotice.php"> Create Notice</a>
+						</div>
+					
+						<div class="menu1">
+							<a href="#"> View Complain</a>
+						</div>
+						<div class="menu1">
+							<a href="display.php">View Student Details</a>
+						</div>
+						<!-- <div class="menu1">
+							<a href="#">Edit Student Information</a>
+						</div> -->
+						<div class="menu1">
+							<a href="BMInfo.php"> Board Member Profile</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-10" id="right-column" style="padding-top: 50px">
+
+				
---
+++ display.php
@@ -0,0 +1,133 @@
+<?php
+	include_once '../Authentication/dbh.db.php';
+
+	$sql = "SELECT * FROM studentfullinformation";
+	$result = mysqli_query($conn, $sql);
+	$resultCheck = mysqli_num_rows($result);
+
+	
+?>
+<?php
+	include 'BMHeader.php';
+?>
+		<div class="col-lg-12"> 
+			<div>
+				<div class="col-lg-8"><h2> Student Information </h2></div>
+				<div class="col-lg-4">
+					<form class="navbar-form navbar-left">
+					    <div class="input-group">
+							<input type="text" class="form-control"  id="myInput" placeholder="Search Student">
+							<div class="input-group-btn">
+							    <button class="btn btn-default" type="submit">
+							        <i class="glyphicon glyphicon-search"></i>
+							    </button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<table class="table table-striped" id="myTable1" style="background-color: rgba(9, 138, 150,0.7);">
+				<thead>
+					<tr>
+						<th onclick="sortTable(0)" style="cursor: pointer;">Reg No.</th>
+						<th onclick="sortTable(1)" style="cursor: pointer;">Name</th>
+						<th onclick="sortTable(2)" style="cursor: pointer;">Session</th>
+						<th onclick="sortTable(3)" style="cursor: pointer;">Department</th>
+						<th>Roll</th>
+						<th>Present Address</th>
+						<th>Permanent Address</th>
+						<th>Blood Group</th>
+						<th onclick="sortTable(4)" style="cursor: pointer;">Status</th>
+						<th colspan="2">Action</th>
+					</tr>								
+				</thead>
+			
+
+				<?php
+					while ($row=mysqli_fetch_assoc($result)) {?>
+						<tbody  id="myTable">
+							<tr>
+								<td><?php echo $row['regNo']; ?></td>
+								<td><?php echo $row['name'];?></td>
+								<td><?php echo $row['session'];?></td>
+								<td><?php echo $row['dept']; ?></td>
+								<td><?php echo $row['classRoll']; ?></td>
+								<td><?php echo $row['presentAddress'];?></td>
+								<td><?php echo $row['permanentAddress']; ?></td>
+								<td><?php echo $row['bloodGroup']; ?></td>
+								<td><?php echo $row['status']; ?></td>
+								<td>
+									<a href='editStudentDetails.php?edit=<?php echo $row['id']; ?>' class='edit_btn'>Edit</a>
+								</td>
+								<td>
+									<a href='DeleteStudent.php?delete=<?php echo $row['id']; ?> ' class='del_btn'>Delete</a>
+								</td>
+							</tr>
+						</tbody>
+				<?php }?>
+			</table>
+		</div>					
+	</div>
+	<script>
+
+		function sortTable(n) {
+		  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+		  table = document.getElementById("myTable1");
+		  switching = true;
+		  //Set the sorting direction to ascending:
+		  dir = "asc"; 
+		  while (switching) {
+
+		    switching = false;
+		    rows = table.getElementsByTagName("TR");
+
+		    for (i = 1; i < (rows.length - 1); i++) {
+		      
+		      shouldSwitch = false;
+		      
+		      x = rows[i].getElementsByTagName("TD")[n];
+		      y = rows[i + 1].getElementsByTagName("TD")[n];
+		    
+		      if (dir == "asc") {
+		        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		          shouldSwitch= true;
+		          break;
+		        }
+		      } else if (dir == "desc") {
+		        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+		          shouldSwitch = true;
+		          break;
+		        }
+		      }
+		    }
+		    if (shouldSwitch) {
+		     
+		      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		      switching = true;
+		      //Each time a switch is done, increase this count by 1:
+		      switchcount ++;      
+		    } else {
+		      /*If no switching has been done AND the direction is "asc",
+		      set the direction to "desc" and run the while loop again.*/
+		      if (switchcount == 0 && dir == "asc") {
+		        dir = "desc";
+		        switching = true;
+		      }
+		    }
+		  }
+		}
+		$(document).ready(function(){
+		  $("#myInput").on("keyup", function() {
+		    var value = $(this).val().toLowerCase();
+		    $("#myTable tr").filter(function() {
+		      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		    });
+		  });
+		});
+
+		
+	</script>
+<?php
+	include 'BMFooter.php';
+?>					
+
